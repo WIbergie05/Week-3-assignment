fc0599d7db55c750932762137f9bbfafc0939dca
WIbergie05 <119378975+WIbergie05@users.noreply.github.com>
2023-01-06 09:33:41 -0600

    adding

--- Week3CodingAssignment.js
+++ Week3CodingAssignment.js
@@ -1,26 +1,57 @@
  var ages = [3, 9, 23, 64, 2, 8, 28, 93]
-console.log(ages);
+console.log("1.", ages);
 //  let x = ages[0];
 //  let y = ages[7];
 //  console.log(y - x);
+// var first = ages.at(0);
+// console.log(first);
 
- for (let i = 0; i < 93; i++){
-     console.log(ages[i] - (ages.length-1));
+// var first = ages.at(0);
+// var last = ages.at(-1);
+// console.log(last);
+// console.log(first);
+// console.log(last - first);
+console.log("1.a.", ages[ages.length - 1] - ages[0]);
+
+//  for (let index = 0; index < ages.length; index++){
+//     console.log("index:", index, "arraylength", ages.length);
+//  }
+
+ ages.push(19);
+ console.log("1.b.", ages[ages.length - 1] - ages[0]);
+
+
+function calculateAverage(ages) {
+    var total = 0;
+    var count = 0;
+    ages.forEach(function(item, index){
+        total += item;
+        count++;
+    });
+    return total / count;
+}
+console.log("1.c.", calculateAverage(ages));
+
+var names = ['Sam', 'Tommy', 'Tim', 'Sally', 'Buck', 'Bob']
+console.log("2.", names);
+
+function calculateAverage(namespar) {
+    var total = namespar.length;
+    var count = 0;
+    for (let index = 0; index < namespar.length; index++){
+    console.log("index:", index, "arraylength", namespar.length, namespar[index].length);
+    count += namespar[index].length
+    console.log(count);
  }
+    return count / total;
+}
+console.log("2.a.", calculateAverage(names));
 
-//  ages.push(15, 19);
-//  console.log(ages);
+// // console.log
 
+// //  console.log(names[names.length -1]);
+// //  console.log(names[0]);
 
-
-
-// var names = ['Sam', 'Tommy', 'Tim', 'Sally', 'Buck', 'Bob']
-
-// console.log
-
-//  console.log(names[names.length -1]);
-//  console.log(names[0]);
-
-//  var nameLengths
+// //  var nameLengths
 
 
